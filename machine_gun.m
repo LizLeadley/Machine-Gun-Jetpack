29c57ab5d876d54cd4020fa648b75b77904fc71b
Miranda Lao <mlao@olin.edu>
2016-11-29 16:12:49 -0500

    updated with bullet casing loss

--- machine_gun.m
+++ machine_gun.m
@@ -1,4 +1,4 @@
-function [T,S] = machine_gun(speed)
+function [T,S] = machine_gun(num_bullets)
 
 %limits
 %Muzzle velocity
@@ -9,27 +9,28 @@ mass = 149; %kg
 
 %Mass of bullet
 mass_bullet = .390; %kg
+mass_casing = .442; %kg
 
+mass_btotal = mass_bullet + mass_casing; %kg
 
 %Load 50 bullets worth of mass
-mass = mass + (50*mass_bullet);
+mass = mass + (num_bullets*mass_btotal);
 
 
 %initial position
 y = 0;
 Timee(1) = 0;
 Y_pos(1) = y;
-%speed into initial velocity
-v_y = speed;
+
   
 options = odeset('Events', @peak_reached, 'RelTol', 1e-4);
 
 hold on
-for i = 0:50
-% %Velocity of gun after thrust’s effect
-% v_y = (-mass_bullet*v_muzzle)/(mass);
-% %Remove mass of one bullet from mass of gun
-% mass = mass - mass_bullet;
+for i = 0:num_bullets
+%Velocity of gun after thrust’s effect
+v_y = (-mass_bullet*v_muzzle)/(mass);
+%Remove mass of one bullet from mass of gun
+mass = mass - mass_btotal;
 
 [T,S] = ode45(@machine_gun_function, [0,20], [y,v_y],options);
 
