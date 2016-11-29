296e9bef82ff42dd365a9efff733b2be3603e2ff
Elizabeth Leadley <eleadley@olin.edu>
2016-11-29 17:07:53 -0500

    Changed from single bullet to firing rate

--- machine_gun.m
+++ machine_gun.m
@@ -1,38 +1,39 @@
-function [T,S] = machine_gun(num_bullets)
+function [T,S] = machine_gun(num_bullets, firing_rate)
 
-%limits
+%% Variables and Conditions
+
 %Muzzle velocity
 v_muzzle = -845; %m/s
-
 %Initial mass of gun alone
 mass = 149; %kg
-
 %Mass of bullet
 mass_bullet = .390; %kg
 mass_casing = .442; %kg
-
 mass_btotal = mass_bullet + mass_casing; %kg
+
 
 %Load 50 bullets worth of mass
 mass = mass + (num_bullets*mass_btotal);
-
 
 %initial position
 y = 0;
 Timee(1) = 0;
 Y_pos(1) = y;
 
-  
-options = odeset('Events', @peak_reached, 'RelTol', 1e-4);
+%% ODE45 time, with loop
+
+% options = odeset('Events', @peak_reached, 'RelTol', 1e-4);
 
 hold on
 for i = 0:num_bullets
+    
 %Velocity of gun after thrust’s effect
 v_y = (-mass_bullet*v_muzzle)/(mass);
 %Remove mass of one bullet from mass of gun
 mass = mass - mass_btotal;
 
-[T,S] = ode45(@machine_gun_function, [0,20], [y,v_y],options);
+% [T,S] = ode45(@machine_gun_function, [0,20], [y,v_y],options);
+[T, S] = ode45(@machine_gun_function, [0, 1/firing_rate], [y, v_y]);
 
 Y = S(:,1);
 
@@ -44,12 +45,19 @@ Y_pos = [Y_pos;Y];
 y = Y(end);
 end
 
-plot(Timee,Y_pos);
+%% Plotting
 
-    function [value, isterminal, direction] = peak_reached(~,W)
-        v_y_current = W(2);
-        value = v_y_current;
-        isterminal = 1;
-        direction = 0;
-    end
+plot(Timee,Y_pos);
+xlabel ('Time (seconds)')
+ylabel ('Vertical Height (meters)')
+title ('GSH-6-30 Firing Vertically')
+legend ('10000 Bullets', '1000 Bullets')
+
+% %% Events Function
+%     function [value, isterminal, direction] = peak_reached(~,W)
+%         v_y_current = W(2);
+%         value = v_y_current;
+%         isterminal = 1;
+%         direction = 0;
+%     end
 end
