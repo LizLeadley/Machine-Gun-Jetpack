74571b13015857af1be3ddb87379a066c2397d10
Elizabeth Leadley <eleadley@olin.edu>
2016-12-03 13:52:54 -0500

    Messing with mountain imaging

--- Git_Learning.m
+++ Git_Learning.m
@@ -2,4 +2,11 @@
 Header comments
 %}
 
-HaddockSuck = 0;
+% HaddockSuck = 0;
+
+sides = 3;
+ t = (1/(2*sides):1/(sides):1)*2*pi-pi/3;
+ b = 3;
+ x = b*sin(t)+5/8*xmax;
+ y = b*cos(t)+3/4*ymax;
+ fill(x,y,'black');
